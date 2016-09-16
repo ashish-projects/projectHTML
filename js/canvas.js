e28c30ed87ab149d814a0fbc30f40a8c9e98123d
Ashish Raina <ashish.raina@bookmyshow.com>
2016-09-16 19:47:14 +0530

    completed canvas page

--- js/canvas.js
+++ js/canvas.js
@@ -1,8 +1,20 @@
+function drawDiagonals (){
+	var c = document.getElementById("canvas1");
+	var ctx = c.getContext("2d");
+	ctx.moveTo(0,0);
+	ctx.lineTo(300,300);
+	ctx.moveTo(300,0);
+	ctx.lineTo(0,300);
+	ctx.stroke();
+}
 
+drawDiagonals();
+
+ // second canvas starts here
 
 function drawRandomLines (){
 
-	var c = document.getElementById("canvas1");
+	var c = document.getElementById("canvas2");
 	var ctx = c.getContext("2d");
 
 	var number1 = 100 * Math.random();
@@ -18,27 +30,67 @@ function drawRandomLines (){
 
 }
 
-function DisplayRandomText (alphabet, xaxis){
+function displayRandomText (alphabet, xaxis,yaxis){
 
-	var c = document.getElementById("canvas1");
+	var c = document.getElementById("canvas2");
 	var ctx = c.getContext("2d");
 	ctx.font ="30px arial";
-	ctx.fillText (alphabet,xaxis,100);
+	ctx.strokeText (alphabet,xaxis,yaxis);
 }
 
+displayRandomText("randomText",10,50);
+setInterval(drawRandomLines, 100);
 
-//setInterval(drawRandomLines, 100);
+//third canvas starts here
 
+function drawCircle (){
+	var c = document.getElementById("canvas3");
+	var ctx = c.getContext("2d");
+	ctx.beginPath();
+	ctx.arc(150,150,50,0,2*Math.PI);
+	ctx.stroke();
+}
 
-function writeName (){
-	var name = [ "a", "s" , "h" , "i", "s" , "h" ] ;
-	for (var i=0; i< name.length; i++){
-		 var x = 100 + 25 * i;
-		 // console.log(i);
-		 console.log(name[i]);
-		DisplayRandomText (name[i], x);
+drawCircle();
 
-	}
+function writeName(){
+	var c = document.getElementById("canvas4");
+	var ctx = c.getContext("2d");
+	ctx.font = "30px arial"
+	ctx.strokeText("ashish", 10, 50);
 }
 
 writeName();
+
+function linearGradient (){
+	var c = document.getElementById("canvas5");
+	var ctx = c.getContext("2d");
+
+	// create gradient
+	var grd = ctx.createLinearGradient(0,0,300,300);
+	grd.addColorStop(0,"white");
+	grd.addColorStop(1,"green");
+
+	ctx.fillStyle = grd;
+	ctx.fillRect(0,0,300,300);
+}
+
+linearGradient();
+
+function radialGradient (){
+
+var c = document.getElementById("canvas6");
+var ctx = c.getContext("2d");
+
+var grd = ctx.createRadialGradient(150,150,150,150,150,0);
+grd.addColorStop(0,"red");
+grd.addColorStop(1,"white");
+
+// Fill with gradient
+ctx.fillStyle=grd;
+ctx.fillRect(0,0,300,300);
+
+}
+
+radialGradient();
+
